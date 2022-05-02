c4db9125b7ca1808e40f9bf156a48e8f70809f4b
ndom91 <yo@ndo.dev>
2022-05-02 19:35:16 +0200

    fix: check for write access to .env file

--- create-env.js
+++ create-env.js
@@ -9,7 +9,7 @@ if (process.env.NEXTAUTH_SECRET) {
 (async () => {
   const randomString = randomBytes(16).toString('hex')
   try {
-    await access("./.env", constants.R_OK)
+    await access("./.env", constants.R_OK && constants.W_OK)
     await appendFile('./.env', `\nNEXTAUTH_SECRET=${randomString}\n`)
   } catch (error) {
     await writeFile('./.env', `NEXTAUTH_SECRET=${randomString}\n`)
