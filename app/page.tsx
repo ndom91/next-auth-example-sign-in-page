87470d850f29d16e6750f248ba54c3d1bbf45634
ndom91 <yo@ndo.dev>
2024-04-17 15:29:29 +0200

    fix: separate signin/signout btns

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,36 @@ export default async function Home() {
   const session = await auth();
   return (
     <main className="flex overflow-hidden flex-col justify-between items-center p-24 min-h-screen">
-      <div className="z-10 justify-between items-center w-full max-w-5xl font-mono text-sm lg:flex">
-        <form
-          action={async () => {
-            "use server";
-            session?.user ? await signIn() : await signOut();
-          }}
-        >
-          <button
-            className="flex fixed top-0 left-0 justify-center pt-8 pb-6 w-full bg-gradient-to-b border-b border-gray-300 lg:static lg:p-4 lg:w-auto lg:bg-gray-200 lg:rounded-xl lg:border from-zinc-200 backdrop-blur-2xl lg:dark:bg-zinc-800/30 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
-            type="submit"
+      <div className="z-10 justify-between items-center w-full max-w-5xl lg:flex">
+        {session?.user ? (
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
           >
-            {session?.user ? "Signout" : "Signin"}
-          </button>
-        </form>
+            <button
+              className="flex fixed top-0 left-0 justify-center pt-8 pb-6 w-full bg-gradient-to-b border-b border-gray-300 lg:static lg:p-4 lg:w-auto lg:bg-gray-200 lg:rounded-xl lg:border from-zinc-200 backdrop-blur-2xl lg:dark:bg-zinc-800/30 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
+              type="submit"
+            >
+              Signout
+            </button>
+          </form>
+        ) : (
+          <form
+            action={async () => {
+              "use server";
+              await signIn();
+            }}
+          >
+            <button
+              className="flex fixed top-0 left-0 justify-center pt-8 pb-6 w-full bg-gradient-to-b border-b border-gray-300 lg:static lg:p-4 lg:w-auto lg:bg-gray-200 lg:rounded-xl lg:border from-zinc-200 backdrop-blur-2xl lg:dark:bg-zinc-800/30 dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
+              type="submit"
+            >
+              Signin
+            </button>
+          </form>
+        )}
         <div className="flex fixed bottom-0 left-0 justify-center items-end w-full h-48 bg-gradient-to-t from-white via-white lg:static lg:bg-none dark:from-black dark:via-black lg:size-auto">
           <a
             className="flex gap-2 place-items-center p-8 pointer-events-none lg:p-0 lg:pointer-events-auto"
@@ -39,9 +55,11 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="flex relative place-items-center z-[-1] before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px] flex-col items-start backdrop-blur bg-white/5 rounded-md">
-        <h3 className="p-2 pb-2 w-full rounded-t-md bg-white/10">Session</h3>
-        <pre className="p-2 pt-2">{JSON.stringify(session, null, 2)}</pre>
+      <div className="flex relative place-items-center z-[-1] before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+        <div className="flex-col items-start rounded-md backdrop-blur-xl bg-white/10">
+          <h3 className="p-4 w-full rounded-t-md bg-white/10">Session</h3>
+          <pre className="p-4">{JSON.stringify(session, null, 2)}</pre>
+        </div>
       </div>
 
       <div className="grid mb-32 text-center lg:grid-cols-4 lg:mb-0 lg:w-full lg:max-w-5xl lg:text-left">
